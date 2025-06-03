9204c9d53ff20cedb6e5d0dcea546b1e301753c5
Armaan <amahaj5@uwo.ca>
2025-06-02 20:10:58 -0400

    Albert Einstein POC and layout working.

--- src/App.tsx
+++ src/App.tsx
@@ -3,56 +3,70 @@ import './App.css'
 
 import Graph from "graphology";
 import { SigmaContainer, useLoadGraph } from "@react-sigma/core";
-import { useLayoutCircular } from '@react-sigma/layout-circular';
+import { useLayoutNoverlap } from '@react-sigma/layout-noverlap';
+import { useLayoutForceAtlas2 } from '@react-sigma/layout-forceatlas2'
 import "@react-sigma/core/lib/style.css";
 
 const sigmaStyle = { height: "100px", width: "100px"}
 
-export const LoadGraph = () => {
-  const { positions, assign } = useLayoutCircular();
-  const loadGraph = useLoadGraph();
-
-  useEffect(() => {
-    const graph = new Graph();
-    graph.addNode("first", {x: 0, y: 0, size: 5, label: "My first node", color: "#FA4F40"});
-    graph.addNode("second", {x: 0, y: 0, size: 5, label: "My second node", color: "#FA4F40"});
-    graph.addNode("third", {x: 0, y: 0, size: 5, label: "My second node", color: "#FA4F40"});
-    graph.addNode("fourth", {x: 0, y: 0, size: 5, label: "My second node", color: "#FA4F40"});
-    graph.addNode("fifth", {x: 0, y: 0, size: 5, label: "My second node", color: "#FA4F40"});
-    loadGraph(graph);
-
-    requestAnimationFrame(() => {
-      assign();
-    })
-  }, [loadGraph, assign])
-
-  return null 
-}
 
 
 function App() {
-  const [internalLinks, setInternalLinks] = useState(null)
+  const [parseJson, setParseJson] = useState(null)
   const [searchInput, setSearchInput] = useState(null)
-  useEffect(() => {
-    console.log(searchInput)
-    fetch(`https://en.wikipedia.org/w/api.php?action=parse&page=${searchInput}&prop=links&format=json&origin=*`)
-      .then(res => res.json())
-      .then(setInternalLinks)
-    console.log(internalLinks)
-  }, [searchInput])
+  const [buttonClicked, setButtonClicked] = useState(false)
 
+  useEffect(() => {
+    if (buttonClicked) {
+    fetch(`https://en.wikipedia.org/w/api.php?action=parse&page=Albert_Einstein&prop=links&format=json&origin=*`)
+      .then(res => res.json())
+      .then(setParseJson)
+    }
+  }, [buttonClicked])
+
+
+
+  const LoadGraph = () => {
+    const { positions, assign } = useLayoutForceAtlas2();
+    const loadGraph = useLoadGraph();
+    useEffect(() => {
+      const graph = new Graph();
+      graph.addNode("Albert Einstein", {x: 1, y: 0, size: 5, color: "#FFFFFF", label: 'Albert Einstein', labelColor: '#00FFFF'})
+
+      const linksArray = parseJson ? parseJson.parse.links : [];
+      console.log('Iterating through links')
+      let base = 0;
+      for (let link of linksArray) {
+        console.log(link)
+        graph.addNode(link['*'], {x: base, y: base, size: 5, color: "#FFFFFF", label: link['*']})
+        graph.addEdge('Albert Einstein', link['*'])
+        base++;
+      }
+      console.log('Done adding to graph')
+      
+      loadGraph(graph);
+
+      requestAnimationFrame(() => {
+        assign();
+      })
+      console.log('Done assigning')
+    }, [loadGraph, assign])
+
+    return null 
+  }
 
   return (
-    <>
-      <div>
-        <input onInput={(e) => setSearchInput(e.target.value)}></input>
+    <div className='w-[100vw] h-[100vh] flex flex-col'>
+      <div className='flex flex-row justify-center'>
+        <input className='bg-white' onInput={(e) => setSearchInput(e.target.value)}></input>
+        <button onClick={() => setButtonClicked(!buttonClicked)}>Button</button>
       </div>
-      <div>
-        <SigmaContainer style={sigmaStyle} settings={{ allowInvalidContainer: true }}>
+      <div className='flex-1/2 bg-black'>
+        <SigmaContainer style={{ backgroundColor: 'black', color: 'blue'}} settings={{ allowInvalidContainer: true, labelColor: { color: "#00FFFF"} }}>
           <LoadGraph/>
         </SigmaContainer>
       </div>
-    </>
+    </div>
   )
 }
 
