9012176acc658c0881ae5cbbf69b54b58ec20a70
Armaan <amahaj5@uwo.ca>
2025-06-12 00:56:45 -0400

    sync

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import './App.css'
 
+import { MaxPriorityQueue } from '@datastructures-js/priority-queue';
+
 import Graph from "graphology";
 import { SigmaContainer, useLoadGraph } from "@react-sigma/core";
 import { useLayoutNoverlap } from '@react-sigma/layout-noverlap';
@@ -15,6 +17,7 @@ function App() {
   const [parseJson, setParseJson] = useState(null)
   const [searchInput, setSearchInput] = useState(null)
   const [buttonClicked, setButtonClicked] = useState(false)
+  const [stats, setStats] = useState()
 
   useEffect(() => {
     if (buttonClicked) {
@@ -24,32 +27,65 @@ function App() {
     }
   }, [buttonClicked])
 
-
-
   const LoadGraph = () => {
-    const { positions, assign } = useLayoutForceAtlas2();
+    const { assign } = useLayoutForceAtlas2();
     const loadGraph = useLoadGraph();
+    
+    // https://en.wikipedia.org/w/api.php?action=query&prop=pageviews&titles=Article1|Article2|Article3&format=json
+
+    // Create graph and add nodes
     useEffect(() => {
       const graph = new Graph();
       graph.addNode("Albert Einstein", {x: 1, y: 0, size: 5, color: "#FFFFFF", label: 'Albert Einstein', labelColor: '#00FFFF'})
 
       const linksArray = parseJson ? parseJson.parse.links : [];
-      console.log('Iterating through links')
+      const encodedLinks = linksArray.map(item => {
+        return encodeURIComponent(item['*'].replace(/ /g, '_'))
+      })
+
+      function* batchArray<T>(array: T[], batchSize: number): Generator<T[]> {
+        for (let i = 0; i < array.length; i += batchSize) {
+          yield array.slice(i, i + batchSize);
+        }
+      }
+      
+      let viewArticleMap = {}
+      for (let batch of batchArray(encodedLinks, 50)) {
+        let articleList = batch.join('|')
+        fetch(`https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=pageviews&titles=${articleList}&format=json`)        
+          .then(res => res.json())
+          .then(setStats)
+        if (stats) {
+          const pageIds = Object.keys(stats.query.pages)
+          const pages = stats.query.pages
+          for (let pageId of pageIds) {
+            const page = pages[pageId]
+            const pageViews = page.pageviews
+            const dates = Object.keys(pageViews)
+            dates.sort().pop()
+            const latest = dates.pop()
+            const viewCount = pageViews[latest]
+            if (!viewArticleMap[viewCount]) {
+              viewArticleMap[viewCount] = [page.title]
+            }
+            else {
+              viewArticleMap[viewCount].push(page.title)
+            }
+            // TODO: Filter by Template: and Wikipedia:
+          }
+        }
+      }
+      console.log(viewArticleMap)
+      
       let base = 0;
       for (let link of linksArray) {
-        console.log(link)
         graph.addNode(link['*'], {x: base, y: base, size: 5, color: "#FFFFFF", label: link['*']})
         graph.addEdge('Albert Einstein', link['*'])
         base++;
       }
-      console.log('Done adding to graph')
       
       loadGraph(graph);
-
-      requestAnimationFrame(() => {
-        assign();
-      })
-      console.log('Done assigning')
+      assign();
     }, [loadGraph, assign])
 
     return null 
